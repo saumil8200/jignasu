8af789a69bd4f273eaea3ba33e0b48b491d190cf
Jignasu <1jignasu@gmail.com>
2022-12-17 10:31:09 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      Welcome!
+    // <div className="App">
+    //   Welcome!
+    // </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/test" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    <div>
+      Hello!
     </div>
+    </>
   );
 }
 
